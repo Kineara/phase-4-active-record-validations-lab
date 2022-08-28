466d50df7233c00ad4248f72d9d57a932e97eaf7
Lauren Hoock <laurenh8916@protonmail.com>
2022-08-28 17:01:47 -0600

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "is not included in the list"}
+  validate :title_is_clickbait
+
+  def title_is_clickbait
+    if title
+      unless title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top") || title.include?("Guess")
+        errors.add(:title, "no")
+      end
+    end
+  end
+
+
 end
